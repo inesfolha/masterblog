49c9d356ab02aa4bacb91e69813aa9a08f9b034e
inesfolha <ines.folha98@gmail.com>
2023-06-14 17:38:57 +0100

    deleting feature added

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for
-import json
+import uuid
 from masterblog.static import file_handler
 
 app = Flask(__name__)
+
+
+def id_generator():
+    return str(uuid.uuid4())
 
 
 @app.route('/')
@@ -18,7 +22,9 @@ def add():
         title = request.form.get('title')
         content = request.form.get('content')
         blog_posts = file_handler.load_file('blog_posts.json')
+        post_id = id_generator()
         new_post = {
+            'id': post_id,
             'author': author,
             'title': title,
             'content': content
@@ -29,6 +35,20 @@ def add():
 
         return redirect(url_for('index'))
     return render_template('add.html')
+
+@app.route('/delete/<string:id>', methods=['GET', 'POST'])
+def delete(id):
+    blog_posts = file_handler.load_file('blog_posts.json')
+    post_to_delete = None
+    for post in blog_posts:
+        if post['id'] == id:
+            post_to_delete = post
+            break
+
+    if post_to_delete:
+        blog_posts.remove(post_to_delete)
+        file_handler.save_file('blog_posts.json', blog_posts)
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
