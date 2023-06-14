22f0614fd5f7d088a4b51e2b0b6b07f321e24db1
inesfolha <ines.folha98@gmail.com>
2023-06-15 00:52:26 +0100

    features: updating and likes added

--- app.py
+++ app.py
@@ -27,7 +27,8 @@ def add():
             'id': post_id,
             'author': author,
             'title': title,
-            'content': content
+            'content': content,
+            'likes': 0
         }
         blog_posts.append(new_post)
 
@@ -36,18 +37,61 @@ def add():
         return redirect(url_for('index'))
     return render_template('add.html')
 
-@app.route('/delete/<string:id>', methods=['GET', 'POST'])
-def delete(id):
+
+@app.route('/delete/<string:post_id>', methods=['GET', 'POST'])
+def delete(post_id):
     blog_posts = file_handler.load_file('blog_posts.json')
     post_to_delete = None
     for post in blog_posts:
-        if post['id'] == id:
+        if post['id'] == post_id:
             post_to_delete = post
             break
 
     if post_to_delete:
         blog_posts.remove(post_to_delete)
         file_handler.save_file('blog_posts.json', blog_posts)
+    return redirect(url_for('index'))
+
+
+@app.route('/update/<string:post_id>', methods=['GET', 'POST'])
+def update(post_id):
+
+    blog_posts = file_handler.load_file('blog_posts.json')
+    post_to_update = None
+    for post in blog_posts:
+        if post['id'] == post_id:
+            post_to_update = post
+            break
+    if post_to_update is None:
+        return "Post not found", 404
+
+    if request.method == 'POST':
+
+        updated_author = request.form['author']
+        updated_title = request.form['title']
+        updated_content = request.form['content']
+
+        post_to_update['author'] = updated_author
+        post_to_update['title'] = updated_title
+        post_to_update['content'] = updated_content
+
+        file_handler.save_file('blog_posts.json', blog_posts)
+        return redirect(url_for('index'))
+
+    return render_template('update.html', post=post_to_update)
+
+
+@app.route('/like/<string:post_id>', methods=['POST'])
+def like(post_id):
+    blog_posts = file_handler.load_file('blog_posts.json')
+    for post in blog_posts:
+        if post['id'] == post_id:
+            if 'likes' in post:
+                post['likes'] += 1
+            else:
+                post['likes'] = 1
+            break
+    file_handler.save_file('blog_posts.json', blog_posts)
     return redirect(url_for('index'))
 
 
